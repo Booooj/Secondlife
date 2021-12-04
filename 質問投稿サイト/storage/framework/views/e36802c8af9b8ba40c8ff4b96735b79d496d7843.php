f5049210a3c7f0ff9de91532b05c2f3044ed848d
Booooj <90837337+Booooj@users.noreply.github.com>
2021-12-04 10:14:56 +0900

    Delete 質問投稿サイト/storage directory

--- 質問投稿サイト/storage/framework/views/e36802c8af9b8ba40c8ff4b96735b79d496d7843.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('footer'); ?>
-<footer style="padding:200px">
-        <p class="text">©質問投稿サイト</p>
-    </div>
-</footer>
-<?php $__env->stopSection(); ?><?php /**PATH C:\Users\cdgam\Desktop\github\QAサイト\resources\views/layout/qafooter.blade.php ENDPATH**/ ?>
--- 質問投稿サイト/storage/framework/views/fefa26a92fc57a063969f83503a1e66fd11e2eaf.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('header'); ?>
-<header class="text">
-        <a style="font-size:35px"href="<?php echo e(url('/home')); ?>" style="padding-left:50px">質問投稿サイト</a>
-    </div>
-    
-</header>
-<?php $__env->stopSection(); ?><?php /**PATH C:\Users\cdgam\Desktop\github\QAサイト\resources\views/layout/qaheader.blade.php ENDPATH**/ ?>
