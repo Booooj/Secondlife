8b687d81d7ed7d16323746f3db5cfeb42f7d6c30
Booooj <90837337+Booooj@users.noreply.github.com>
2021-12-04 10:12:49 +0900

    Add files via upload

---
+++ 質問投稿サイト/storage/framework/views/e080c678a910e40902723bd61b92243da9097547.php
@@ -0,0 +1,37 @@
+ 
+<?php $__env->startSection('title', '質問編集ページ'); ?>
+ 
+<?php echo $__env->make('layout.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+ 
+<?php $__env->startSection('content'); ?>
+<div class="text">
+<h1 style="padding:30px">
+  質問内容を編集する
+</h1>
+<form method="post" action="<?php echo e(url('/posts', $post->id)); ?>">
+  <?php echo e(csrf_field()); ?>
+
+  タイトル
+  <p>
+    <input type="text" name="title" value="<?php echo e(old('title', $post->title)); ?>">
+    <?php if($errors->has('title')): ?>
+    <span><?php echo e($errors->first('title')); ?></span>
+    <?php endif; ?>
+  </p>
+  質問内容
+  <p>
+    <textarea name="content"><?php echo e(old('content', $post->content)); ?></textarea>
+    <?php if($errors->has('content')): ?>
+    <span><?php echo e($errors->first('content')); ?></span>
+    <?php endif; ?>
+  </p>
+  <p>
+    <input type="submit" value="変更" class="btn btn-success btn-sm">
+    <a href="<?php echo e(url('/home')); ?>"> <button class="btn btn-warning btn-sm">戻る</button></a>
+  </p>
+</form>
+</div>
+<?php $__env->stopSection(); ?>
+ 
+<?php echo $__env->make('layout.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('layout.mainlayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\cdgam\Desktop\github\QAサイト\resources\views/posts/edit.blade.php ENDPATH**/ ?>
---
+++ 質問投稿サイト/storage/framework/views/ef4b96e8be91844edf7c01b0f9cfde52acbb457d.php
@@ -0,0 +1,7 @@
+<?php $__env->startSection('header'); ?>
+<header class="text">
+        <a style="font-size:35px"href="<?php echo e(url('/home')); ?>" style="padding-left:50px">質問投稿サイト</a>
+    </div>
+    
+</header>
+<?php $__env->stopSection(); ?><?php /**PATH C:\Users\cdgam\Desktop\QAサイト\resources\views/layout/qaheader.blade.php ENDPATH**/ ?>
