2d790f6c0e2385d3d8cf5c5679c92de68aff2c72
Booooj <90837337+Booooj@users.noreply.github.com>
2021-12-12 09:45:40 +0900

    Delete app directory

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  before_action :login_user
-
-  def top
-    @posts = Post.all.order(created_at: :desc)
-  end
-  
-end
-
--- app/helpers/consalts_helper.rb
+++
@@ -1,2 +0,0 @@
-module ConsaltsHelper
-end
