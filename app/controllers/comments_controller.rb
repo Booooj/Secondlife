0181d49bb29c0443ff97e32c096652864b3b9d32
Booooj <90837337+Booooj@users.noreply.github.com>
2021-12-12 17:12:24 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,9 @@ class CommentsController < ApplicationController
   
     def create
         @post = Post.find(params[:post_id])
-        @post.comments.create(comment_params)
+        @comment = Comment.new(comment_params)
+        @comment.user_id = current_user.id
+        @comment.save
         redirect_to post_path(@post)
       end
     
