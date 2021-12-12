81a9c59a87973b0c6cb22bd50478ca90ee5bbbcb
Booooj <90837337+Booooj@users.noreply.github.com>
2021-12-12 09:47:15 +0900

    Delete test directory

--- test/controllers/consalts_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ConsaltsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get consalts_index_url
-    assert_response :success
-  end
-end
